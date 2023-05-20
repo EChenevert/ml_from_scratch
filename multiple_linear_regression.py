b5df9f54f1bb34e7069b6447f1e6439301aa3298
EChenevert <99147101+EChenevert@users.noreply.github.com>
2023-05-20 17:31:30 -0400

    working on separating categorical variable and cooks distance

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -19,38 +19,44 @@ class LinearRegression:
         self.p_value_model = None  # from the f_statistic
 
 
-    def fit(self, x_train, y_train, tails=2):
+    def fit(self, x_train, y_train, tails=2, category=None):
         """
 
         NOTE: I invert the matrix, so this will take a while if the number of samples is large
         :param X_train:
         :param y_train:
         :param tails: default is a 2 tail test
+        :param category: a string of the column name in a dataframe that holds categorical variable
         :return: Nahhhhh
         """
-        x_train = np.column_stack((np.ones(len(x_train[:, 0])), x_train))
-        weights = np.linalg.inv(x_train.T @ x_train) @ (x_train.T @ y_train)
-        self.weight_vector = weights[1:]
-        self.bias_term = np.mean(weights[0])
+        if category == None:
+            x_train = np.column_stack((np.ones(len(x_train[:, 0])), x_train))
+            weights = np.linalg.inv(x_train.T @ x_train) @ (x_train.T @ y_train)
+            self.weight_vector = weights[1:]
+            self.bias_term = np.mean(weights[0])
 
-        # Compute f-statistic and associated p_value
-        residuals = y_train - np.dot(x_train, weights)
-        RSS = np.sum(residuals**2)
-        TSS = np.sum((y_train - np.mean(y_train))**2)
-        dof_numerator = len(x_train[0, :]) - 1  # the degrees of freedom of the numerator of f_stat eq
-        dof_denominator = len(x_train[:, 0]) - dof_numerator  # degrees of freedom of denominator of f_stat eq
-        numerator = RSS/dof_numerator
-        denominator = TSS/dof_denominator
-        self.f_statistic = numerator/denominator
-        self.p_value = 1 - stats.f.cdf(self.f_statistic, dof_numerator, dof_denominator)
+            # Compute f-statistic and associated p_value
+            residuals = y_train - np.dot(x_train, weights)
+            RSS = np.sum(residuals**2)
+            TSS = np.sum((y_train - np.mean(y_train))**2)
+            dof_numerator = len(x_train[0, :]) - 1  # the degrees of freedom of the numerator of f_stat eq
+            dof_denominator = len(x_train[:, 0]) - dof_numerator  # degrees of freedom of denominator of f_stat eq
+            numerator = RSS/dof_numerator
+            denominator = TSS/dof_denominator
+            self.f_statistic = numerator/denominator
+            self.p_value = 1 - stats.f.cdf(self.f_statistic, dof_numerator, dof_denominator)
 
-        # Compute t_statistics and assocaited p_values
-        mse = RSS / dof_numerator  # a.k.a variance of residuals
-        self.cov_matrix = mse * np.linalg.inv(x_train.T @ x_train)
-        standard_errors = np.sqrt(np.diag(self.cov_matrix))
-        self.t_statistics = weights / standard_errors  # includes the t_statistic of the bias term
-        self.p_values = tails * (1 - stats.t.cdf(np.abs(self.t_statistics), df=dof_denominator))
-
+            # Compute t_statistics and assocaited p_values
+            mse = RSS / dof_numerator  # a.k.a variance of residuals
+            self.cov_matrix = mse * np.linalg.inv(x_train.T @ x_train)
+            standard_errors = np.sqrt(np.diag(self.cov_matrix))
+            self.t_statistics = weights / standard_errors  # includes the t_statistic of the bias term
+            self.p_values = tails * (1 - stats.t.cdf(np.abs(self.t_statistics), df=dof_denominator))
+        else:
+            pass
+            # extract categorical column
+            # make x_train a dataframe, this will be easier for associating coefficents to var names
+            # save the categorical column for future use in the Cook's distance for importance
 
     def predict(self, x_test):
         """
@@ -59,6 +65,8 @@ class LinearRegression:
         :return: predictions of the target
         """
         return np.dot(x_test, self.weight_vector) + self.bias_term
+
+
 
 
 
