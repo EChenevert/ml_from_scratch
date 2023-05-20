28f2327bb241474e3e50f3dcabdab242dfe38d15
EChenevert <99147101+EChenevert@users.noreply.github.com>
2023-05-20 13:27:17 -0400

    added predict, simplified fit

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -4,23 +4,45 @@ import numpy as np
 class LinearRegression:
     """
 
+    Sources:
+    https://rowannicholls.github.io/python/statistics/hypothesis_testing/multiple_linear_regression.html
+
     """
     def __init__(self):
         self.weight_vector = None
         self.bias_term = None
+        self.p_values = None
+        self.f_statistic = None
+        self.t_statistics = None
 
-    def fit(self, X_train, y_train, classes=None):
+    def fit(self, x_train, y_train):
         """
 
+        NOTE: I invert the matrix, so this will take a while if the number of samples is large
         :param X_train:
         :param y_train:
         :param classes: default is None, if specified, specify column index within matrix
         :return: Nahhhhh
         """
-        if classes == None:
-            X_train = np.column_stack((np.ones(len(X_train[:, 0])), X_train))
-            weights = np.linalg.inv(X_train.T @ X_train) @ (X_train.T @ y_train)
-            self.weight_vector = weights[1:]
-            self.bias_term = np.mean(weights[0])
-        else:
-            pass
+        X_train = np.column_stack((np.ones(len(x_train[:, 0])), x_train))
+        weights = np.linalg.inv(X_train.T @ X_train) @ (X_train.T @ y_train)
+        self.weight_vector = weights[1:]
+        self.bias_term = np.mean(weights[0])
+
+        # Compute p-values and f-statistic
+        residuals = y_train - np.dot(x_train, weights)
+        dof = len(x_train) - weights
+        mse = np.sum(residuals**2) / dof  # a.k.a variance of residuals
+
+
+
+    def predict(self, x_test):
+        """
+
+        :param X_test:
+        :return: predictions of the target
+        """
+        return np.dot(x_test, self.weight_vector) + self.bias_term
+
+
+
---
+++ pca.py
@@ -0,0 +1,10 @@
+# Notes:
+# 1. two methods: maximizing variance formulation OR minimizing error formulation (both yield same algorithm)
+# 2. Should include an option to have user defined number of PCs OR choose ideal number of PCs from the data
+# 3. PCA can be used for the compression of a dataset, for data preproccessing (in which data is normalizated
+# and de-correlated, and it can be used in data visualization. My class must have all these!
+# 4. I should include a derivation of the mathematical logic, following bishop, in Github (mostly as not to myself
+# and coolness factor).
+# 5. I should include an option of "probabilistic PCA", this probably helps with choosing the number of dimensions
+# when reducing dimensionality
+# ---> But this will need an EM algorithm
